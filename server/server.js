e9c1d23d3c557502203591941a86a4fc2122ec3d
abhin <abhinavrathore.ar@gmail.com>
2023-05-02 01:55:34 +0530

    server file edited for debugging issue

--- server/server.js
+++ server/server.js
@@ -2,12 +2,15 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const PORT = process.env.PORT || 8080;
-const cors = require('cors');
+const cors = require("cors");
 
 app.use(cors());
 
 app.use(express.static(path.join(__dirname, "../client/build")));
 
+console.log(path.join(__dirname, "../client/build"));
+
 app.get('*', (req, res) => res.sendFile(path.join(__dirname, "../client/build/index.html")));
+console.log(path.join(__dirname, "../client/build/index.html"));
 
 app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
